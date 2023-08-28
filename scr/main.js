44058be7c2c952d97c089d7d853d26b0a8659915
Jorge Vera <jorgevera988@gmail.com>
2023-08-28 11:15:35 -0500

    buscadoroculto

---
+++ scr/main.js
@@ -0,0 +1,7 @@
+const searchHTML = document.querySelector(".search")
+const btnHTML = document.querySelector(".btn")
+const inputHTML = document.querySelector(".input")
+
+btnHTML.addEventListener("click", () => {
+    searchHTML.classList.toggle("active")
+inputHTML.focus})
